7e3239d4983981441c44335a315e00aff006fdf9
Masoud Ramezani <masoudhra@gmail.com>
2023-06-21 14:10:55 +1200

    start practice project

---
+++ src/context/ProductContextProvider.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+//API
+import { getProducts } from '../services/api';
+
+const ProductContextProvider = () => {
+
+    useEffect(() => {
+
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProductContextProvider;
