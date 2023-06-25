55f3d1a42184610548f1d8260762340449e1ebeb
Masoud Ramezani <masoudhra@gmail.com>
2023-06-25 14:47:10 +1200

    commit som try to practice

--- src/context/ProductContextProvider.js
+++ src/context/ProductContextProvider.js
@@ -1,57 +1,63 @@
-// import React, { useState, useEffect, createContext } from 'react';
-
-// //API
-// import { getProducts } from '../services/api';
-
-
-// const ProductsContext = createContext();
-
-// const ProductContextProvider = (props) => {
-
-//     const [products, setProducts] = useState([]);
-
-//     useEffect(() => {
-
-//         const fetchAPI = async() =>{
-//             setProducts(await getProducts());
-//         }
-//         fetchAPI();
-
-//     },[])
-
-
-//     return (
-//        <ProductContextProvider value={products}>
-//             {props.children}
-
-//        </ProductContextProvider>
-//     );
-// };
-
-// export default ProductContextProvider;
-
 import React, { useState, useEffect, createContext } from 'react';
-import { getProducts } from '../services/api'
+
+//API
+import { getProducts } from '../services/api';
+
+
+const ProductsContext = createContext();
 
 const ProductContextProvider = (props) => {
 
-    const [products, setProducts] = useState ([]);
-
-    const Productsontext = createContext()
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        const fetchAPI = async() => {
-            setProducts (await getProducts())
+
+        const fetchAPI = async() =>{
+            setProducts(await getProducts());
         }
-        fetchAPI()
+        fetchAPI();
+
     },[])
 
+
     return (
-        <ProductContextProvider>
+       <ProductContextProvider value={products}>
             {props.children}
 
-        </ProductContextProvider>
+       </ProductContextProvider>
     );
 };
 
 export default ProductContextProvider;
+
+// import React, { useState, useEffect, createContext } from 'react';
+// import { getProducts } from '../services/api';
+
+
+
+// const ProductsContext = createContext();
+
+
+
+
+// const ProductContextProvider = (children) => {
+
+// const [products, setProducts] = useState ([]);
+
+// useEffect (() => {
+
+//     const fetchAPI = async() => {
+//         setProducts (await getProducts())
+//     }
+//     fetchAPI();
+
+// },[])
+
+//     return (
+//         <ProductContextProvider value={products}>
+//             {children}
+//         </ProductContextProvider>
+//     );
+// };
+
+// export default ProductContextProvider;
